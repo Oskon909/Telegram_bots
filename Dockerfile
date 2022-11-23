4e669d1c98d5d93b12717114f45d94af2de1cd4d
oskon <akul6999@gmail.com>
2022-11-23 13:21:16 +0600

    docker

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+# syntax=docker/dockerfile:1
+FROM python:3.10
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
+WORKDIR /code
+RUN pip install --upgrade pip
+COPY requirements.txt /code/
+RUN pip install -r requirements.txt
+COPY . /code/
+CMD [ "python3", "-m" , "--host=0.0.0.0"]
--- bot.py
+++ bot.py
@@ -15,13 +15,13 @@ logging.basicConfig(
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Hello,I'm a bot!")
-    await context.bot.send_document(chat_id=update.effective_chat.id, document=open('test', 'rb'))
+    # await context.bot.send_document(chat_id=update.effective_chat.id, document=open('test', 'rb'))
 
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token('5315599976:AAFQo11VxD-jRAF9tVC6oNEqsfDowvGq1QQ').build()
 
-    start_handler = CommandHandler('start', start)
+    start_handler = CommandHandler('Hello', start)
     application.add_handler(start_handler)
 
     application.run_polling()
